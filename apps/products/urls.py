46a94b0ee9725dad4e3443aea57f089f01d6c6fd
Hall Enoch Asanda <hallenochasanda@gmail.com>
2026-02-19 05:13:29 +0300

    hotfix: updating the requirements file with required python django packages

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Internal routes
     path('', views.ProductListCreateAPIView.as_view(), name='product-list-create'),
     path('<uuid:id>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
     path('<uuid:pk>/approve/', views.approve_product_view, name='product-approve'),
     
-    # Public routes
-    # public/ prefix handled in main urls.py usually, but if grouped here:
-    # We'll expose public views here too or via a separate 'public/' include
+    path('public/', views.PublicProductListAPIView.as_view(), name='product-list-public'),
+    path('public/<uuid:id>/', views.PublicProductDetailAPIView.as_view(), name='product-detail-public'),
 ]
